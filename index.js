8b220946f6b9ded14b7e898a140450dafaeb0f4b
sana perween <sanaperween@sanas-MacBook-Pro.local>
2026-07-17 19:13:59 +0530

    Add Zod validation and improve authentication

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const jwt=require("jsonwebtoken");
+const bcrypt=require("bcrypt");
 const JWT_SECRET="sana123"
 const {UserModel , TodoModel}=require ("./db")
 const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://sana602perween_db_user:sana12345@cluster0.z8crdti.mongodb.net/todosana-22");
+const {z}=require("zod");
+mongoose.connect("mongodb+srv://sana602perween_db_user:daniyal602@cluster0.z8crdti.mongodb.net/todosana-22");
 
 mongoose.connection.once("open", () => {
     console.log("Connected to:", mongoose.connection.name);
@@ -13,32 +15,69 @@ app.use(express.json());
 //signup
 app.post("/signup",async function(req,res)
 {
+    const requiredbody=z.object({
+        email:z.string(),
+        name:z.string(),
+        password:z.string()
+    })
+    const parsedDataWithSuccess=requiredbody.safeParse(req.body);
+    if(!parsedDataWithSuccess.success){
+        res.json({
+            msg:"incorrect format"
+        })
+        return 
+    }
 const email=req.body.email;
 const password=req.body.password;
 const name=req.body.name;
+let errorThrown=false;
+try{
+const hashedpassword=await bcrypt.hash(password,5);
+console.log(hashedpassword);
 await UserModel.create({
     email:email,
-    password:password,
+    password:hashedpassword,
     name:name
-})
-res.json({
-    msg:"you are logged in"
-})
+});
+// throw new error("user already exists")
+} catch (e) {
+    
+
+    res.json({
+        msg: "user already exist"
+    });
+
+    errorThrown = true;
+}
+if(!errorThrown){
+    res.json({
+        msg:"you are signed up"
+    })
+}
+
 })
 //signin
 app.post("/signin",async function(req,res){
      const email=req.body.email;
      const password=req.body.password;
-     const user=await UserModel.findOne({
-        email: email,
-        password: password
+     const response =await UserModel.findOne({
+        email: email
+        
      })
-     console.log(user);
-     if(user)
-     {
+     console.log(response);
+     if(!response){
+        res.status(403).json({
+            msg:"user does not exist in our DataBase"
+        })
+        return
+     }
+
+     const passwordmatch=await bcrypt.compare(password,response.password);
+     if(passwordmatch){
+     
         const token=jwt.sign(
             {
-                id:user._id.toString()
+                id:response._id.toString()
             },JWT_SECRET);
         
         res.json({
@@ -67,7 +106,7 @@ res.json({
 app.get("/todos",auth ,function(req,res)
 {
   const userId=req.userId;
-  const todos= TodoModel.find({
+  const todos=  TodoModel.find({
     userId:userId
   })
   res.json({
